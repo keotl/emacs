fa92ee9a8e1f8e12dcae997e5f783680d78aa0db
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-11-29 09:29:41 -0500

    tweak theme + projectile

--- bootstrap.el
+++ bootstrap.el
@@ -5,6 +5,7 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(global-hl-line-mode 1)
 
 (setq visible-bell t)
 
--- vcs.el
+++ vcs.el
@@ -2,3 +2,11 @@
 (keotl/leader-keys
   "g" '(:ignore t :which-key "vcs")
   "gs" 'magit-status)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap ("M-m p" . projectile-command-map)
+  :init (setq projectile-switch-project-action #'projectile-dired)
+  )
+
