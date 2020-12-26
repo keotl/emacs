6a689955586ebce1be6c73637ecac3f9eccd5509
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-12-25 20:02:39 -0500

    ace-window + basic window management

--- keymap.el
+++ keymap.el
@@ -5,13 +5,30 @@
 (global-set-key (kbd "C-a") 'back-to-indentation-or-beginning)
 (setq scroll-error-top-bottom t)
 
+;; Text handling
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+)
+
 ;; Leader keys
 (use-package general
   :init (global-set-key (kbd "M-m") nil)
   :config
   (general-create-definer keotl/leader-keys :prefix "M-m")
-  (keotl/leader-keys
-   "tt" '(counsel-load-theme :which-key "choose theme")))
+  (keotl/leader-keys "tt" '(counsel-load-theme :which-key "choose theme"))
+  (keotl/leader-keys "xld" '(duplicate-line :which-key "duplicate line"))
+  ;; Window management
+  (keotl/leader-keys "wm" '(delete-other-windows :which-key "maximize"))
+  (keotl/leader-keys "w/" '(split-window-right :which-key "split vertically"))
+  (keotl/leader-keys "w-" '(split-window-below :which-key "split horizontally"))
+  (keotl/leader-keys "w+" '(toggle-window-split :which-key "toggle split direction"))
+  )
 
 
 ;; Transient mini-states
@@ -24,3 +41,5 @@
   ("RET" nil "finished" :exit t))
 (keotl/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+
--- ui.el
+++ ui.el
@@ -57,3 +57,43 @@
 ;; Treemacs
 (use-package treemacs
   :after lsp)
+
+
+;; ace-window
+(use-package ace-window
+  :init (setq aw-keys '(?a ?u ?i ?e ?, ?c ?t ?s ?r))
+  :bind
+  (("C-x o" . ace-window)))
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+
+;; Default split behaviour: vertical line
+(setq split-width-threshold nil)
+;; Default split behaviour: horizontal line
+;; (setq split-width-threshold 1)
+
+
