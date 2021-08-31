94646d8de67e65f67e9d1d4df2d201596cea87a3
Kento A. Lauzon <kento.lauzon@ligature.ca>
2021-08-31 06:28:01 -0400

    f# snippets

--- lang/fsharp.el
+++ lang/fsharp.el
@@ -1,2 +1,9 @@
 (use-package fsharp-mode
-  :hook (fsharp-mode . lsp-deferred))
+  :hook (fsharp-mode . lsp-deferred)
+  :hook (fsharp-mode . lsp-lens-mode)
+  :hook (fsharp-mode . smartparens-mode)
+  :config (lsp-lens-mode)
+  :bind ("M-m m p a" . fsharp-add-this-file-to-proj)
+  :bind ("M-m m p r" . fsharp-remove-this-file-from-proj)
+  :config (bind-key* "C-c C-f" 'lsp-format-buffer) ;; late-bind override
+  )
