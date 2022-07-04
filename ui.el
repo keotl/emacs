003bc291117297f594528bc005a6b4f4f26ad830
Kento Lauzon <kento.lauzon@ligature.ca>
2022-07-04 12:58:43 -0400

    fix treemacs shortcut

--- ui.el
+++ ui.el
@@ -56,7 +56,7 @@
 
 ;; Treemacs
 (use-package treemacs
-  :bind (("M-m g t" . treemacs-display-current-project-exclusively)
+  :bind (("M-m g t" . treemacs-select-window)
 	 ("M-m g T" . treemacs)
 	 )
   )
