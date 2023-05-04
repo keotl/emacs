b18544c5cb9687077ad80fa202fb53ee5ce4a94c
Kento Lauzon <kento.lauzon@ligature.ca>
2023-05-04 08:43:04 +0100

    python formatter

--- lang/python.el
+++ lang/python.el
@@ -1,3 +1,6 @@
+(load  (concat (file-name-directory load-file-name) "../util/py-yapf.el"))
+(load  (concat (file-name-directory load-file-name) "../util/python-isort.el"))
+
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
@@ -5,5 +8,12 @@
                           (lsp-deferred)
 			  (smartparens-mode)
 			  ))
+  :bind (:map python-mode-map ("C-c C-f" . python-reformat))
+  ;; :bind (:map python-mode-map ("C-c C-f" . py-yapf-buffer))
   ;; :bind ("C-x C-<return>" . lsp-find-definition)
   )
+
+(defun python-reformat ()
+  (interactive)
+  (py-isort-buffer)
+  (py-yapf-buffer))
