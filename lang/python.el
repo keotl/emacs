f2d035ea2f5d646d9818da83af336a70b63019b9
Kento Lauzon <kento@lauzon.co.uk>
2025-02-14 08:05:56 +0000

    add python formatting

--- lang/python.el
+++ lang/python.el
@@ -1,3 +1,6 @@
+(load  (concat (file-name-directory load-file-name) "../vendor/py-yapf.el"))
+(load  (concat (file-name-directory load-file-name) "../vendor/python-isort.el"))
+
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
                '(python-mode . ("~/.emacs.d/.cache/lsp/npm/pyright/bin/pyright-langserver" "--stdio")))
