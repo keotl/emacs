1f5b7a5beba214d6dfd5ff4c736bae66ac00a251
Kento Lauzon <kento@lauzon.co.uk>
2025-08-21 08:51:02 +0100

    disable trailing whitespace in vterm

--- apps.el
+++ apps.el
@@ -1,4 +1,10 @@
 (use-package vterm
   :ensure t
   :bind (:map vterm-mode-map ("M-q" . vterm-send-next-key))
+  :config
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'vterm-mode 'insert))
+  :init
+  (add-hook 'vterm-mode-hook
+            (lambda () (setq-local show-trailing-whitespace nil)))
   )
